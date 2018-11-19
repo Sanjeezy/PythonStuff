af0f241bc579ef9e4d50a21875ead8f7ca564c9c
sanjay tamizharasu <sanjay.tamizharasu@workday.com>
2018-11-19 15:41:34 -0800

    added templates

--- simple_app.py
+++ simple_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -7,12 +8,30 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')	#<name> signifies passing in a query to the route, without using ? in URL
 def index(name="Treehouse"):
+	return render_template("index.html",name=name)
+
+
 	# name = request.args.get('name', name)
-	return "Hello from {}".format(name)
+	#return "Hello from {}".format(name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 def add(num1, num2):
-	return '{} + {} = {}'.format(num1, num2, num1+num2)
+	return render_template("add.html", num1=num1, num2=num2)
+
+
+
+
+
+
+	# return """
+	# <!doctype html>
+	# <html>
+	# <Head><title>Adding</title></head>
+	# <body>
+	# <h1>{} + {} = {}</h1>
+	# </body>
+	# </html>
+	# """.format(num1, num2, num1+num2)
 
 
 
